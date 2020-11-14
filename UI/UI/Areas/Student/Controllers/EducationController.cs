e84bc66c03436e2ff1855147670abdac2b18c06c
MUSTAFA avcı <m.avci1974@hotmail.com>
2020-11-14 15:55:24 +0300

    kk

---
+++ UI/UI/Areas/Student/Controllers/EducationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Entity;
+
+namespace UI.Areas.Student.Controllers
+{
+    public class EducationController : Controller
+    {
+        EduContext db = new EduContext();
+
+        // GET: Student/Education
+        public ActionResult Index(int? id)
+        {
+            Lesson lesson = db.Lessons.Find(id);
+            return View(lesson);
+        }
+    }
+}
