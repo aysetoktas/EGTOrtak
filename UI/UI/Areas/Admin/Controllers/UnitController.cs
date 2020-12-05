6d938d1d07ca5b5f309437f8152263508efd0ef2
Ayşe Toktaş <ayseetoktas@hotmail.com>
2020-12-05 19:46:03 +0300

    updddd

--- UI/UI/Areas/Admin/Controllers/UnitController.cs
+++ UI/UI/Areas/Admin/Controllers/UnitController.cs
@@ -32,6 +32,29 @@ namespace UI.Areas.Admin.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            Unit updUnit = db.Units.Find(id);
+            return View(updUnit);
+        }
+        [HttpPost]
+        public ActionResult Update(Unit data)
+        {
+            Unit updUnit = db.Units.Find(data.ID);
+            updUnit.Name = data.Name;
+            updUnit.EducationID = data.EducationID;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Unit delUnit = db.Units.Find(id);
+            db.Units.Remove(delUnit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
     }
 }
