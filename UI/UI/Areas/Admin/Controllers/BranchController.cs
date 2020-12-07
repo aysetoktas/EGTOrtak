68788c29b594045ec4bfd4cabcd0e07e008c1f40
Ayşe Toktaş <ayseetoktas@hotmail.com>
2020-12-08 00:43:58 +0300

    gdgdhg

---
+++ UI/UI/Areas/Admin/Controllers/BranchController.cs
@@ -0,0 +1,35 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Areas.Admin.Controllers
+{
+    public class BranchController : Controller
+    {
+        EduContext db = new EduContext();
+        // GET: Admin/Branch
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Branch data)
+        {
+            Branch yeni = new Branch();
+            yeni.Name = data.Name;
+
+            db.Branches.Add(yeni);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
