00bd4b1537d24c2d0895d969d5fe0516f63c2197
Robotik Bilim <gurkan@robotikbilim.com.tr>
2020-12-25 23:11:16 +0300

    FinalChange

---
+++ UI/Entity/Migrations/202012221949197_mig22122020.cs
@@ -0,0 +1,18 @@
+﻿namespace Entity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig22122020 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Lessons", "ExamLink", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Lessons", "ExamLink");
+        }
+    }
+}
